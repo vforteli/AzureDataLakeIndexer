e2c187e3846cb14101331106d36dbd000f5e458e
Verner Fortelius <verner@fortelius.net>
2023-12-15 23:00:25 +0200

    fix dotnet 8 isolated aot deployt

--- DatalakePathIndexerFunc/publish_to_azure_dev.sh
+++ DatalakePathIndexerFunc/publish_to_azure_dev.sh
@@ -1,5 +1,5 @@
 set -e
 
-dotnet publish --configuration Release
-(cd bin/Release/net8.0/publish && rm -f foo.zip && zip -r foo.zip ./)
-az webapp deployment source config-zip --resource-group search-indexer-testing --name 'datalake-indexer-func-dev' --src ./bin/Release/net8.0/publish/foo.zip
+dotnet publish -c Release --self-contained --runtime linux-x64 -o publish/ 
+(cd publish && rm -f foo.zip && zip -r foo.zip ./)
+az webapp deployment source config-zip --resource-group search-indexer-testing --name 'datalake-indexer-func-dev' --src ./publish/foo.zip
