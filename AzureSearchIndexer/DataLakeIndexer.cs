6e4a745300a6df8fab21b52ad80c06edf34348e0
Verner Fortelius <verner@fortelius.net>
2023-12-26 11:53:52 +0200

    switch to channels for pathbuffer

--- AzureSearchIndexer/DataLakeIndexer.cs
+++ AzureSearchIndexer/DataLakeIndexer.cs
@@ -1,6 +1,7 @@
 using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Net;
+using System.Threading.Channels;
 using Azure.Search.Documents;
 using Azure.Storage.Files.DataLake;
 using Azure.Storage.Files.DataLake.Models;
@@ -22,11 +23,11 @@ public class DataLakeIndexer(SearchClient searchClient, ILogger<DataLakeIndexer>
         Func<PathIndexModel, FileDownloadInfo, Task<TIndex?>> func,
         CancellationToken cancellationToken)
     {
-        var pathsBuffer = new BlockingCollection<PathIndexModel>(options.DocumentBatchSize * options.MaxUploadThreads * 2);
+        var pathsChannel = Channel.CreateBounded<PathIndexModel>(options.DocumentBatchSize * options.MaxUploadThreads * 2);
         var documents = new BlockingCollection<TIndex>(options.DocumentBatchSize * (options.MaxUploadThreads + 2));
 
-        var listPathsTask = ReadPathsAsync(paths, pathsBuffer, cancellationToken);
-        var readDocumentsTask = ReadDocumentsAsync(options, dataLakeServiceClient, func, pathsBuffer, documents, cancellationToken);
+        var listPathsTask = ReadPathsAsync(paths, pathsChannel, cancellationToken);
+        var readDocumentsTask = ReadDocumentsAsync(options, dataLakeServiceClient, func, pathsChannel, documents, cancellationToken);
         var uploadDocumentsTask = batchingUploader.UploadBatchesAsync(documents, searchClient, cancellationToken);
 
         var stopwatch = Stopwatch.StartNew();
@@ -55,7 +56,7 @@ public class DataLakeIndexer(SearchClient searchClient, ILogger<DataLakeIndexer>
         DatalakeIndexerOptions options,
         DataLakeServiceClient dataLakeServiceClient,
         Func<PathIndexModel, FileDownloadInfo, Task<TIndex?>> func,
-        BlockingCollection<PathIndexModel> pathsBuffer,
+        ChannelReader<PathIndexModel> pathsBuffer,
         BlockingCollection<TIndex> documents,
         CancellationToken cancellationToken) => Task.Run(async () =>
         {
@@ -70,36 +71,39 @@ public class DataLakeIndexer(SearchClient searchClient, ILogger<DataLakeIndexer>
             {
                 using var semaphore = new SemaphoreSlim(options.MaxReadThreads, options.MaxReadThreads);
 
-                while (pathsBuffer.TryTake(out var path, Timeout.Infinite, cancellationToken))
+                while (await pathsBuffer.WaitToReadAsync(cancellationToken).ConfigureAwait(false))
                 {
-                    await semaphore.WaitAsync(cancellationToken).ConfigureAwait(false);
-
-                    var taskId = Guid.NewGuid();
-                    readTasks.TryAdd(taskId, Task.Run(async () =>
+                    if (pathsBuffer.TryRead(out var path))
                     {
-                        try
-                        {
-                            var file = await dataLakeServiceClient.GetFileSystemClient(path.filesystem).GetFileClient(WebUtility.UrlDecode(path.pathUrlEncoded)).ReadAsync(cancellationToken).ConfigureAwait(false);
+                        await semaphore.WaitAsync(cancellationToken).ConfigureAwait(false);
 
-                            var document = await func.Invoke(path, file.Value).ConfigureAwait(false);
-                            if (document != null)
+                        var taskId = Guid.NewGuid();
+                        readTasks.TryAdd(taskId, Task.Run(async () =>
+                        {
+                            try
                             {
-                                documents.Add(document);
-                                Interlocked.Increment(ref documentReadCount);
+                                var file = await dataLakeServiceClient.GetFileSystemClient(path.filesystem).GetFileClient(WebUtility.UrlDecode(path.pathUrlEncoded)).ReadAsync(cancellationToken).ConfigureAwait(false);
+
+                                var document = await func.Invoke(path, file.Value).ConfigureAwait(false);
+                                if (document != null)
+                                {
+                                    documents.Add(document);
+                                    Interlocked.Increment(ref documentReadCount);
+                                }
                             }
-                        }
-                        catch (TaskCanceledException) { }
-                        catch (Exception ex)
-                        {
-                            Interlocked.Increment(ref documentReadFailedCount);
-                            logger.LogError(ex, "Failed deserializing document {path}", path.pathUrlEncoded);
-                        }
-                        finally
-                        {
-                            readTasks.TryRemove(taskId, out _);
-                            semaphore.Release();
-                        }
-                    }, cancellationToken));
+                            catch (TaskCanceledException) { }
+                            catch (Exception ex)
+                            {
+                                Interlocked.Increment(ref documentReadFailedCount);
+                                logger.LogError(ex, "Failed deserializing document {path}", path.pathUrlEncoded);
+                            }
+                            finally
+                            {
+                                readTasks.TryRemove(taskId, out _);
+                                semaphore.Release();
+                            }
+                        }, cancellationToken));
+                    }
                 }
 
                 await Task.WhenAll(readTasks.Select(o => o.Value)).ConfigureAwait(false);
@@ -123,16 +127,17 @@ public class DataLakeIndexer(SearchClient searchClient, ILogger<DataLakeIndexer>
 
 
     /// <summary>
-    /// Fill BlockingCollection with paths
+    /// Fill Channel with paths
     /// </summary>   
-    internal static Task ReadPathsAsync(IAsyncEnumerable<PathIndexModel> paths, BlockingCollection<PathIndexModel> pathsBuffer, CancellationToken cancellationToken) =>
+    internal static Task ReadPathsAsync(IAsyncEnumerable<PathIndexModel> paths, ChannelWriter<PathIndexModel> pathsBuffer, CancellationToken cancellationToken) =>
         Task.Run(async () =>
         {
             await foreach (var path in paths)
             {
-                pathsBuffer.Add(path);
+                await pathsBuffer.WaitToWriteAsync().ConfigureAwait(false);
+                await pathsBuffer.WriteAsync(path).ConfigureAwait(false);
             }
 
-            pathsBuffer.CompleteAdding();
+            pathsBuffer.Complete();
         }, cancellationToken);
 }
